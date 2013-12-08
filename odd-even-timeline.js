7f72da4b4e6e5e380d4904b4d9b51bbb2fff932f
mgrsskls <mail@mgrossklaus.de>
2013-12-08 22:00:18 +0100

    changed indention

--- odd-even-timeline.js
+++ odd-even-timeline.js
@@ -1,43 +1,43 @@
 (function($){
 
-	$.fn.oddEvenTimeline = function() {
+  $.fn.oddEvenTimeline = function() {
 
-		return this.each(function() {
+    return this.each(function() {
 
-			init($(this));
-
-
-			function init($list) {
-
-				var $items	= $list.find('.timeline__item:not(.timeline__item--odd,.timeline__item--even)');
-
-				$.each($items, function() {
-					arrangeSingleItem($list, $(this));
-				});
-			}
+      init($(this));
 
 
-			function arrangeSingleItem($list, $item) {
+      function init($list) {
 
-				var $lastLeft							= $list.find('.timeline__item--odd').last(),
-						$lastRight						= $list.find('.timeline__item--even').last(),
-						lastLeftOuterHeight		= $lastLeft.outerHeight(),
-						lastLeftTopPosition		= $lastLeft.offset().top,
-						lastLeftEndPosition		= lastLeftOuterHeight + lastLeftTopPosition,
-						lastRightOuterHeight	= $lastRight.outerHeight(),
-						lastRightTopPosition	= $lastRight.offset().top,
-						lastRightEndPosition	= lastRightOuterHeight + lastRightTopPosition;
+        var $items  = $list.find('.timeline__item:not(.timeline__item--odd,.timeline__item--even)');
 
-				if(lastRightEndPosition < lastLeftEndPosition) {
-					$item.addClass('timeline__item--even');
-				}
-				else {
-					$item.addClass('timeline__item--odd');
-				}
-			}
+        $.each($items, function() {
+          arrangeSingleItem($list, $(this));
+        });
+      }
 
-		});
 
-	};
-	
+      function arrangeSingleItem($list, $item) {
+
+        var $lastLeft             = $list.find('.timeline__item--odd').last(),
+            $lastRight            = $list.find('.timeline__item--even').last(),
+            lastLeftOuterHeight   = $lastLeft.outerHeight(),
+            lastLeftTopPosition   = $lastLeft.offset().top,
+            lastLeftEndPosition   = lastLeftOuterHeight + lastLeftTopPosition,
+            lastRightOuterHeight  = $lastRight.outerHeight(),
+            lastRightTopPosition  = $lastRight.offset().top,
+            lastRightEndPosition  = lastRightOuterHeight + lastRightTopPosition;
+
+        if(lastRightEndPosition < lastLeftEndPosition) {
+          $item.addClass('timeline__item--even');
+        }
+        else {
+          $item.addClass('timeline__item--odd');
+        }
+      }
+
+    });
+
+  };
+  
 }(jQuery));
