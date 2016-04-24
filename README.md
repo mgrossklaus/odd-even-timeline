f7712978d6912b6fd2543da39e166486575e67d6
mgrsskls <mail@mgrossklaus.de>
2016-04-24 20:06:36 +0200

    get rid of jquery, updated readme and license

--- README.md
+++ README.md
@@ -1,10 +1,46 @@
-odd-even-timeline
+Timeline.js
 =================
 
-jQuery Plugin to arrange items like the current facebook timeline does
+**Timeline** is a JavaScript plugin, that arranges items in a given list similar to the old facebook stream. You can see an example at [https://mgrossklaus.github.io/timeline/](https://mgrossklaus.github.io/timeline/).
 
 
-Example
+Usage
 =================
+Include the `timeline.css` and `timeline.js` and use a markup like the following:
 
-http://oddeven.mgrossklaus.de
+```html
+<ul id="timeline">
+  <li class="timeline__item"></li>
+  ...
+  <li class="timeline__item"></li>
+</ul>
+```
+Create an instance of the Timeline like this:
+```javascript
+var timeline = new Timeline(document.getElementById('timeline'));
+```
+Afterwards you can update the list by calling `timeline.updateList();`. This rearranges all the items, what could be useful if you e.g. changed the items content and therefore their height.
+
+After appending a new item `foo` to the list, you can call `timeline.arrangeItem(foo)` to set it to the correct position.
+
+Settings
+=================
+**Timeline** accepts one DOM element as the first parameter and an optional settings object as the second parameter:
+
+```javascript
+var timeline = new Timeline(document.getElementById('timeline'), {
+    item: 'item-class',
+    odd:  'item-class--odd',
+    even: 'item-class--even',
+});
+```
+
+| option | default | explanation |
+|---|---|---|
+| `item` | `timeline__item` | CSS class of all items in a Timeline list element |
+| `odd` | `timeline__item--odd` | CSS class for items that will be put to the left |
+| `even` | `timeline__item--even` | CSS class for items that will be put to the right |
+
+Todos
+=================
+* Updating the list after prepending elements
---
+++ timeline.js
@@ -0,0 +1,87 @@
+/**
+ * Timeline JavaScript Plugin.
+ * Arranges item to the left and the right similar to the previous facebook timeline stream.
+ *
+ * See instructions at: https://github.com/mgrossklaus/timeline
+ * Demo: https://mgrossklaus.github.io/timeline
+ *
+ * @author: Michael Großklaus (https://www.mgrossklaus.de)
+ */
+
+var Timeline = function(list, options) {
+  'use strict';
+
+  var listItems,
+
+      // extend a default set of classes with the assigned ones.
+      classes = extend({
+        item: 'timeline__item',
+        odd:  'timeline__item--odd',
+        even: 'timeline__item--even',
+      }, options);
+
+  listItems = list.querySelectorAll('.' + classes.item);
+
+  // return if we can't find any items to arrange.
+  if (listItems.length === 0) {
+    return;
+  }
+
+  // the first two items should always arranged to
+  // the left and the right.
+  listItems[0].classList.add(classes.odd);
+  listItems[1].classList.add(classes.even);
+
+  updateList();
+
+  /**
+   * arranges all items in a timeline that do not have the either 'odd' or 'even' class
+   * by calling arrangeItem() for each of these items.
+   */
+  function updateList() {
+    var items   = list.querySelectorAll('.' + classes.item + ':not(.' + classes.odd + '):not(.' + classes.even + ')'),
+        itemsL  = items.length;
+
+    for (var i = 0; i < itemsL; i++) {
+      arrangeItem(items[i]);
+    }
+  }
+
+  /**
+   * adds either the 'odd' or the 'even' class to a single item
+   * by comparing the coordinate of the lower edges of the last
+   * odd and the last even item.
+   */
+  function arrangeItem(item) {
+    var oddAll  = list.querySelectorAll('.' + classes.odd),
+        evenAll = list.querySelectorAll('.' + classes.even);
+
+    item.classList.add(
+      oddAll[oddAll.length - 1].getBoundingClientRect().bottom > evenAll[evenAll.length - 1].getBoundingClientRect().bottom ?
+        classes.even : classes.odd
+      );
+  }
+
+  /**
+   * merges two objects and returns a merged object.
+   */
+  function extend(defaultClasses, params) {
+    var extended = defaultClasses;
+
+    for (var prop in params) {
+      if (Object.prototype.hasOwnProperty.call(params, prop)) {
+        extended[prop] = params[prop];
+      }
+    }
+
+    return extended;
+  }
+
+  /**
+   * return functions for the public API.
+   */
+  return {
+    updateList: updateList,
+    arrangeItem: arrangeItem
+  }
+};
